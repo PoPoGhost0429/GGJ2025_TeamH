58c0d711248a29b0a122b0de7bfb585545dc3d92
PoPo <ericisfunny0429@gmail.com>
2025-01-25 13:41:36 +0800

    add CollisionDect

--- Assets/Scripts/Bubble/bubbleController.cs
+++ Assets/Scripts/Bubble/bubbleController.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class bubbleController : BubbleBase{
     // Update is called once per frame
     public float followPlayerSpeed = 1f, absorbSpeed = 0.3f, absorbRatio = 0.8f, absorbMaxDis = 1f, gas=0;
-    //Vector3 test = new Vector3(0, 0, 0);
+    Vector3 test = new Vector3(0, 0, 0);
 
     protected override void Update(){
         base.Update();
@@ -21,12 +21,17 @@ public class bubbleController : BubbleBase{
     }
 
     public void absorption(Vector3 playPos){
-        GetComponent<Rigidbody2D>().AddForce((playPos - transform.position) * followPlayerSpeed);
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
         transform.localScale -= new Vector3(absorbSpeed*absorbRatio, absorbSpeed*absorbRatio, 0);
         if(transform.localScale.x < 0.01f){
             gas = 0;
             gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Player" && gameObject.tag != "Pearl"){
+            GetComponent<Rigidbody2D>().AddForce((other.transform.position - transform.position) * followPlayerSpeed);
         }
     }
 }
