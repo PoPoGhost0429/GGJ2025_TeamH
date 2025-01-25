03835f31daa7425e6a4225f66e59110a25ed3de2
V <j92080412@gmail.com>
2025-01-25 16:33:21 +0800

    [Test] 測試輸入

---
+++ Assets/Scripts/System/Input/TestPlayer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class TestPlayer : MonoBehaviour
+{
+    [SerializeField] private int _Index;
+    
+    private InputData m_InputData;
+
+    private void OnEnable()
+    {
+        if (InputSystem.Instance.PlayerControllers[_Index] != null)
+        {
+            InputSystem.Instance.PlayerControllers[_Index].OnInputEvent += OnMove;
+        }
+    }
+    
+    private void OnDisable()
+    {
+        if (InputSystem.Instance != null && _Index < InputSystem.Instance.PlayerControllers.Length &&
+            InputSystem.Instance.PlayerControllers[_Index] != null)
+        {
+            InputSystem.Instance.PlayerControllers[_Index].OnInputEvent -= OnMove;
+        }
+    }
+
+    private void Update()
+    {
+        transform.Translate(m_InputData.moveDirection * (10 * Time.deltaTime));
+    }
+
+    private void OnMove(InputData obj)
+    {
+        m_InputData = obj;
+    }
+}
